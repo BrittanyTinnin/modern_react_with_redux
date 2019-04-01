ca3267ffac803355f5cbdd56ca90724b9d79bcd6
github username <bntinnin7@gmail.com>
2019-04-01 00:20:42 -0400

    practice inline styling with JSX

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import ReactDOM from 'react-dom';
 
 //create a react component -- functional style
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+    <div>
+        <label class="label" for="name">Enter name:</label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+    );
 };
 
 //Take the react component and show it on the screen
