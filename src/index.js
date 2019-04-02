95188aa0aff5db9a26a55fe971206a1355c3236f
github username <bntinnin7@gmail.com>
2019-04-02 19:07:59 -0400

    quick update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import faker from 'faker'
 
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Good post" />
+            <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="very clean post" />
+            <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Today at 5:00PM" content="Nice blog post!" />
         </div>
     );
 };
