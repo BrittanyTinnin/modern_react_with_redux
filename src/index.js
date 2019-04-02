91f855de59a55ef70bd789c3f5da9505347adb14
github username <bntinnin7@gmail.com>
2019-04-02 15:01:43 -0400

    create functional component for mini comments

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-//import the React and ReactDOM libraries
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
-//create a react component -- functional style
 const App = () => {
-
-const buttonText = { text: 'Click Me!'};
-
     return (
-    <div>
-        <label className="label" htmlFor="name">Enter name:</label>
-        <input id="name" type="text"/>
-        <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
-    </div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
-//Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'))
