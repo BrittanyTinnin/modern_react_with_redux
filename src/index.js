a173ee5cd6027997c60ca01a4417c05c84bebc88
github username <bntinnin7@gmail.com>
2019-04-02 21:42:34 -0400

    pass props from one coponent to another

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
 import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
 
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Good post" />
-            <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="very clean post" />
-            <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Today at 5:00PM" content="Nice blog post!" />
+            <ApprovalCard>
+                <div>
+                    
+                </div>
+                Are you sure you want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Good post" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="very clean post" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Today at 5:00PM" content="Nice blog post!" />
+            </ApprovalCard>
         </div>
     );
 };
